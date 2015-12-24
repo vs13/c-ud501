3c8cc755c3049280d3aaf503f5d469299205269d
Vipul Verma <vipulverma2008@gmail.com>
2015-12-24 13:13:04 +0530

    Plot Multiple Data
    Plot Multiple Data

---
+++ extractmultipledata.py
@@ -0,0 +1,11 @@
+import pandas as pd
+import matplotlib.pyplot as plt 
+
+def test_run():
+	df = pd.read_csv("amzn.csv")
+	print df
+	df[['Close','High']].plot()
+	plt.show()
+
+if __name__ == "__main__":
+	test_run()
