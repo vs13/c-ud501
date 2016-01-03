576e9b6fc3c3dda02ffd0640276652f7057530d6
Vipul Verma <vipulverma2008@gmail.com>
2016-01-03 13:48:23 +0530

    Row Slicing by Columns
    Row Slicing by Columns + Renaming Columns to appropriate names

---
+++ RowSlicingCol.py
@@ -0,0 +1,19 @@
+import pandas as pd
+import matplotlib.pyplot as plt 
+
+def test_run():
+	start_date = '26-Dec-14'
+	end_date = '10-May-15'
+	dates = pd.date_range(start_date,end_date)
+	df = pd.DataFrame(index=dates)
+	for symbol in ['aapl','googl','spy','amzn']:
+		dftmp = pd.read_csv("{}.csv".format(symbol),index_col=0,parse_dates=True)
+		#dftmp = dftmp.rename(columns={'Open':symbol,'Close':symbol,'High':symbol,'Low':symbol,'Volume':symbol})
+		dftmp.columns = ['Open'+symbol,'Close'+symbol,'High'+symbol,'Low'+symbol,'Volume'+symbol]
+		df = df.join(dftmp,how="inner")
+	print df
+	#Slice Rows
+	print df.ix['2015-01-30':'2015-01-01',['Closeamzn']]
+
+if __name__ == "__main__":
+	test_run()
