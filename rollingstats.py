5730a6f2d07a59a6460b7d34a695dae3afb11964
Vipul Verma <vipulverma2008@gmail.com>
2016-01-08 10:13:50 +0530

    Rolling Mean and Numpy updates

--- rollingstats.py
+++ rollingstats.py
@@ -6,6 +6,7 @@ def test_run():
 	end_date = '10-May-15'
 	dates = pd.date_range(start_date,end_date)
 	df = pd.DataFrame(index=dates)
+	#print df
 	for symbol in ['aapl','googl','spy','amzn']:
 		dftmp = pd.read_csv("{}.csv".format(symbol),index_col=0,parse_dates=True)
 		#dftmp = dftmp.rename(columns={'Open':symbol,'Close':symbol,'High':symbol,'Low':symbol,'Volume':symbol})
@@ -14,34 +15,17 @@ def test_run():
 	#print df
 	#plot_df(normalized_data(df.ix['2015-01-30':'2015-01-01',['Closeamzn','Closegoogl','Closeaapl']]))
 	#Slice Rows
-	dftest = df.ix['2015-01-30':'2015-01-01',['Closeamzn']]
+	dftest = df.ix['2015-03-30':'2015-01-01',['Closeamzn','Closegoogl','Closeaapl']]
 	print dftest
-	print np.array([2,3,4])
-	print np.array([(2,3,4),(5,6,7)])
-	print np.ones((5,4))
-	print np.zeros((5,4))
-	print np.ones((5,4) , dtype=np.int_)
-	print np.random.random((5,4))
-	print np.random.rand(5,4)
-	print np.random.normal(size=(2,3)) #Mean 0 SD = 1 
-	print np.random.normal(50,10,size=(2,3)) # Mean 50 SD = 10
-	print np.random.randint(10)
-	print np.random.randint(0,10)
-	print np.random.randint(0,10,size=3)
-	print np.random.randint(0,10,size=(2,3))
-	a = np.random.randint(0,10,size=(2,3))
-	print a.shape
-	print a.shape[0]
-	print a.shape[1]
-	print a.size
-	print a.dtype
-	print a.sum()
-	print a 
-	print a.sum(axis=0) 
-	print a.sum(axis=1) 
-	print "Maximum of each coloumn:\n", a.max(axis=0)
-	print "Mean of all the elements:\n",a.mean()
-	
+	rm = pd.rolling_mean(dftest['Closeamzn'],window=20)
+	#print rm
+	ax = dftest['Closeamzn'].plot(title="AMZN Rolling Mean",label='AMZN')
+	rm.plot(label="Rolling Mean",ax=ax)
+	ax.set_xlabel("Date")
+	ax.set_ylabel("Price")
+	ax.legend(loc='upper left')
+	#plt.show()
+
 def plot_df(df,title="Stock Prices"):
 	ax = df.plot(title=title)
 	ax.set_xlabel("Date")
