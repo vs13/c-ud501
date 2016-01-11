d930edc9f0f27ff310a05432c1e9b0f24e2c8f48
Vipul Verma <vipulverma2008@gmail.com>
2016-01-11 14:36:56 +0530

    Scatter Plots and More

---
+++ scatterplot2.py
@@ -0,0 +1,71 @@
+import pandas as pd
+import matplotlib.pyplot as plt 
+import numpy as np
+def test_run():
+	start_date = '26-Dec-14'
+	end_date = '10-May-15'
+	dates = pd.date_range(start_date,end_date)
+	df = pd.DataFrame(index=dates)
+	#print df
+	for symbol in ['aapl','googl','spy','amzn']:
+		dftmp = pd.read_csv("{}.csv".format(symbol),index_col=0,parse_dates=True)
+		#dftmp = dftmp.rename(columns={'Open':symbol,'Close':symbol,'High':symbol,'Low':symbol,'Volume':symbol})
+		dftmp.columns = ['Open'+symbol,'Close'+symbol,'High'+symbol,'Low'+symbol,'Volume'+symbol]
+		df = df.join(dftmp,how="inner")
+	#df['Date'] =pd.to_datetime(df.Date)
+	#print df.columns
+	df = df.sort_index()
+	#print df
+	
+	#plot_df(normalized_data(df.ix['2015-01-01':'2015-01-30',['Closeamzn','Closegoogl','Closeaapl']]))
+	#Slice Rows
+	dftest = df.ix['2015-01-01':'2015-03-30',['Closeamzn','Closegoogl','Closeaapl']]
+	'''
+	rm_amzn = get_rolling_mean(dftest['Closeamzn'],window=20)
+	rstd_amzn = get_rolling_std(dftest['Closeamzn'],window=20)
+	upper_band,lower_band = get_bollinger_bands(rm_amzn,rstd_amzn)
+	ax = dftest['Closeamzn'].plot(title="Bollinger Bands",label='AMZN')
+	rm_amzn.plot(label="Rolling Mean",ax=ax)
+	upper_band.plot(label="Upper Band",ax=ax)
+	lower_band.plot(label="Lower Band",ax=ax)
+	ax.legend(loc='upper left')
+	ax.set_xlabel
+	#plt.show()
+	'''
+	dfreturn = compute_daily_returns(dftest)
+	#plt.legend(loc='upper right')
+	dfreturn.plot(kind='scatter',x='Closeamzn',y='Closegoogl')
+	beta_amzn,alpha_amzn = np.polyfit(dfreturn['Closeamzn'],dfreturn['Closegoogl'],1)
+	plt.plot(dfreturn['Closeamzn'],beta_amzn*dfreturn['Closeamzn']+alpha_amzn,'-',color='r')
+	plt.show()
+	print dfreturn.corr(method='pearson')
+def plot_df(df,title="Stock Prices"):
+	ax = df.plot(title=title)
+	ax.set_xlabel("Date")
+	ax.set_ylabel("Price")
+	plt.show()
+
+def normalized_data(df):
+	print df/df.ix[0,:]
+	return df/df.ix[0,:]
+
+def compute_daily_returns(df):
+	daily_returns = df.copy()
+	#daily_returns[1:] = (df[1:]/df[:-1].values) - 1
+	daily_returns  = (df/df.shift(1)) - 1 # Alteranate Method
+	daily_returns.iloc[0] = 0 # Set values at 0th row to zero
+	return daily_returns
+
+def get_rolling_mean(values,window):
+	return pd.rolling_mean(values,window=window)
+
+def get_rolling_std(values,window):
+	return pd.rolling_std(values,window=window)
+
+def get_bollinger_bands(rm,rstd):
+	upper_band = rm + rstd*2
+	lower_band = rm - rstd*2
+	return upper_band,lower_band
+
+if __name__ == "__main__":
+	test_run()
